db98d77886a2dd9dd522d79f22c99a753e1dd32c
Ver-yu <udina-vera-vera@yandex.ru>
2025-04-09 22:01:19 +0300

    Update admin.py
    в админку добавлены посты

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post, Poll, PollOption, Vote, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'created_at', 'text_short')
+    def text_short(self, obj):
+        return obj.text[:50] + '...' if len(obj.text) > 50 else obj.text
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post_short', 'text_short')
+    def post_short(self, obj):
+        return obj.post.text[:30] + '...'
+    def text_short(self, obj):
+        return obj.text[:30] + '...'
+
+admin.site.register([Poll, PollOption, Vote])
