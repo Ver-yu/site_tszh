886f315bf6bc7520667cd50c266eb87fb33f3d96
Ksenia-Ermakova <ksyushenka_yermakova@mail.ru>
2025-04-13 21:42:31 +0300

    Update models.py

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+class Ticket(models.Model):
+    STATUS_CHOICES = [
+        ('open', 'В обработке'),
+        ('accepted', 'Принято'),
+        ('closed', 'Завершено'),
+    ]
+
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sent_tickets')
+    receiver = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='received_tickets')
+    description = models.TextField()
+    photo = models.ImageField(upload_to='tickets/photos/', blank=True, null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='open')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Заявка #{self.id} от {self.author.username}"
