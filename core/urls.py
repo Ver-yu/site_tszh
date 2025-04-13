fef504d1afec6038b8902a382c568db8cdd9fff6
Ksenia-Ermakova <ksyushenka_yermakova@mail.ru>
2025-04-13 21:45:19 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('admin/', admin.site.urls),
     path('forum/', include('forum.urls')),
+    path('tickets/', include('tickets.urls')),
 ]
