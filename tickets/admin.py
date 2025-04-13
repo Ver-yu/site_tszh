03688ffed061ef78ecd1b1669d1d0e285668c94b
Ksenia-Ermakova <ksyushenka_yermakova@mail.ru>
2025-04-13 21:41:10 +0300

    Update admin.py

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Ticket
 
-# Register your models here.
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'receiver', 'status', 'created_at')
+    list_filter = ('status',)
+    search_fields = ('author__username', 'receiver__username')
