251dbeb18c3879cbe2dfbcc30478163fe3161eb7
Ver-yu <udina-vera-vera@yandex.ru>
2025-04-14 09:26:53 +0300

    Delete forum/templatetags directory
    удалило

--- forum/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-# forum/templatetags/custom_filters.py
-from django import template
-
-register = template.Library()
-
-@register.filter
-def filter_apartment(votes, apartment):
-    return votes.filter(apartment=apartment).exists()
