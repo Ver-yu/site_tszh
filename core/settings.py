d9a2940390df2799e67025655310f8c427b58959
Ver-yu <udina-vera-vera@yandex.ru>
2025-04-16 00:26:21 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'forum',  # это для форума
-    'profanity_check', # это для форума
+    'forum',  # для форума
     'tickets',
 ]
 
