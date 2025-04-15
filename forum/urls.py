e6ec03cc03c5174cec9e6f8f2646e7aa6809e8ce
Ver-yu <udina-vera-vera@yandex.ru>
2025-04-15 08:33:54 +0300

    Update urls.py
    убила фильтрацию мата

--- forum/urls.py
+++ forum/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
-from .views import (
-    ForumView,
-    create_post,  # Импорт функции
-    vote,
-    add_comment,
-    DeletePostView
-)
+from .views import ForumView, create_post, vote, add_comment, DeletePostView
 
 urlpatterns = [
     path('', ForumView.as_view(), name='forum'),
-    path('create/', create_post, name='create_post'),  # Используем функцию
+    path('create/', create_post, name='create_post'),
     path('poll/<int:poll_id>/vote/', vote, name='vote'),
     path('post/<int:post_id>/comment/', add_comment, name='add_comment'),
     path('delete/<int:pk>/', DeletePostView.as_view(), name='delete_post'),
